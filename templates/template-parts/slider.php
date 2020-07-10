70fbbe4d8c85d528cd93f03ae69121d840efb429
Horatiu <horatiubanciu11@gmail.com>
2020-07-10 15:48:57 +0300

    First push
    Am adaugat fisierele care au fost create pana pe data de 10.07

---
+++ templates/template-parts/slider.php
@@ -0,0 +1,33 @@
+<div class="slider-domenii">
+            <div>
+                <h1 class=""> Data Scientist </h1>
+                <p> 
+                    Data scientists work closely with business stakeholders to understand their goals 
+                      and determine how data can be used to achieve those goals.
+                </p>
+
+            </div>
+            <div>
+                <h1>Database Administrator </h1>
+                <p> A database administrator is someone who uses software to store and organize data,
+                     such as financial information and customer shipping records.
+
+                </p>
+            </div>
+
+            <div>
+                <h1>Entrepreneur </h1>
+                <p>
+                    An entrepreneur is a person who organizes and operates a business or businesses,
+                     taking on greater than normal financial risks in order to do so.
+                </p>
+            </div>
+            <div>
+                <h1>Graphic Designer </h1>
+                <p> </p>
+            </div> 
+            <div>
+                <h1>Web Developer </h1>
+                <p> </p>
+            </div>
+</div>
